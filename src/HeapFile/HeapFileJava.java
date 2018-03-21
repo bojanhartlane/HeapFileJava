6fe93406c71f98f8553370e9c66219f3e92b13c2
Alber <bojanhartlane@gmail.com>
2018-03-21 21:28:24 +1100

    First iteration
    Splitting data based on "Tab" delimiter and printing it out

--- src/HeapFile/HeapFileJava.java
+++ src/HeapFile/HeapFileJava.java
@@ -1,5 +1,68 @@
 package HeapFile;
 
-public class HeapFileJava {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
+/*
+ * This is a Java program to implement heap file. In the first iteration, this program
+ * will try to read each field value through delimiter and print it separately. 
+ */
+
+public class HeapFileJava {
+	
+	public static void main(String[] args) throws IOException, SocketException, UnknownHostException {
+        //BufferedReader to accept file name as input and to read the .csv file
+		BufferedReader buffFileRead = null, fileName = null;
+		//String to contain value of each line in the .csv file
+        String line = "";
+        //Delimiter for the records. In this case, it's Tab or \t 
+        String csvSplitBy = "\t";
+        //Field names
+        String[] fieldNames = null;
+        
+        try {
+        	//Ask user for the file name
+        	System.out.print("Enter the file name: ");
+        	//Use input as file name and access the file with BufferedReader buffFileRead
+        	fileName = new BufferedReader(new InputStreamReader(System.in));
+        	buffFileRead = new BufferedReader(new FileReader(fileName.readLine()));
+        	//Read the first line, which is the field names
+        	if ((line = buffFileRead.readLine()) != null) {
+        		//Split the fields based on the delimiter
+        		fieldNames = line.split(csvSplitBy);
+        	}
+        	//Print the field names
+        	for (int i = 0; i < fieldNames.length; i++)  {
+        		System.out.print(fieldNames[i] + " ");
+        	}
+        	System.out.println();
+        	//Read until the file reaches last line
+        	while ((line = buffFileRead.readLine()) != null) {
+        		//Split the values in each line based on the delimiter
+        		String[] lines = line.split(csvSplitBy);
+        		String newLine = null;
+        		for (int i = 0; i < lines.length; i++) {
+        			if (lines[i].equals("")) {
+                       	newLine += "NULL";
+                   	}
+                   	if (i == (lines.length - 1)) {
+                   		newLine += lines[i];
+                   	} else {
+                   		newLine += lines[i] + "-";
+                   	}                
+        		}
+        		//Print each line
+        		System.out.println(newLine);
+        		System.out.println();
+        	}
+    	 } catch (FileNotFoundException e) {
+    		 e.printStackTrace();
+    	 }
+	}
+	
 }
