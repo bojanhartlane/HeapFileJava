9033614ee55c6d1d21dfe8309c7162798750817c
Alber <bojanhartlane@gmail.com>
2018-03-24 14:18:17 +1100

    Move files back to default package

--- src/dbload.java
+++ src/dbload.java
@@ -1,5 +1,3 @@
-package heapfile;
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
--- src/test.java
+++ src/test.java
@@ -1,4 +1,4 @@
-package heapfile;
+
 
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
